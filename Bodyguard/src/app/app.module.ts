0cd29bec09135e1a05f8dd32472f11d6f387eb7f
Pana Leonardo <leonardo474lm@gmail.com>
2023-10-30 19:05:51 -0500

    anda

--- Bodyguard/src/app/app.module.ts
+++ Bodyguard/src/app/app.module.ts
@@ -7,16 +7,26 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 //component de service
+import { ServiceListarComponent } from './Componentes/service/service-listar/service-listar.component';
+import { ServiceEditarComponent } from './Componentes/service/service-editar/service-editar.component';
 import { ServiceComponent } from './Componentes/service/service.component';
 //component de bodyguard
 
+import { BodyListarComponent } from './Componentes/bodyguar/body-listar/body-listar.component';
+import { BodyEditarComponent } from './Componentes/bodyguar/body-editar/body-editar.component';
 import { BodyguarComponent } from './Componentes/bodyguar/bodyguar.component';
 //component de payment
 
 import { PaymentComponent } from './Componentes/payment/payment.component';
 //component de specialization
-
 import { SpecializationComponent } from './Componentes/specialization/specialization.component';
+import { SpecializationListarComponent } from './Componentes/specialization/specialization-listar/specialization-listar.component';
+import { SpecializationEditarComponent } from './Componentes/specialization/specialization-editar/specialization-editar.component';
+//component de cliente
+import { ClientComponent } from './Componentes/client/client.component';
+import { ClientListarComponent } from './Componentes/client/client-listar/client-listar.component';
+import { ClientEditarComponent } from './Componentes/client/client-editar/client-editar.component';
+
 
 //component de login
 
@@ -33,15 +43,6 @@ import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatMenuModule} from '@angular/material/menu';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { ClientComponent } from './Componentes/client/client.component';
-import { ClientListarComponent } from './Componentes/client/client-listar/client-listar.component';
-import { ClientEditarComponent } from './Componentes/client/client-editar/client-editar.component';
-import { BodyListarComponent } from './Componentes/bodyguar/body-listar/body-listar.component';
-import { BodyEditarComponent } from './Componentes/bodyguar/body-editar/body-editar.component';
-import { ServiceListarComponent } from './Componentes/service/service-listar/service-listar.component';
-import { ServiceEditarComponent } from './Componentes/service/service-editar/service-editar.component';
-import { SpecializationListarComponent } from './Componentes/specialization/specialization-listar/specialization-listar.component';
-import { SpecializationEditarComponent } from './Componentes/specialization/specialization-editar/specialization-editar.component';
 import { MatTableModule} from '@angular/material/table';//add
 import { MatPaginatorModule} from '@angular/material/paginator';
 @NgModule({
