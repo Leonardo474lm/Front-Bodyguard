58a086bbc07809dc24e2d3837ad1e17c22c9a47c
Cesar7321 <choccataolivera05@gmail.com>
2023-11-08 15:57:43 -0500

    Add listbodyguars

--- Bodyguard/src/app/Componentes/bodyguard-shop/bodyguard-shop.component.ts
+++ Bodyguard/src/app/Componentes/bodyguard-shop/bodyguard-shop.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Bodyguard } from 'src/app/Model/bodyguard';
+import { BodyguardService } from 'src/app/Services/bodyguard.service';
 
 @Component({
   selector: 'app-bodyguard-shop',
   templateUrl: './bodyguard-shop.component.html',
   styleUrls: ['./bodyguard-shop.component.css']
 })
-export class BodyguardShopComponent {
+export class BodyguardShopComponent implements OnInit {
+
+  arrBodyguards:Bodyguard[];
+
+  constructor(
+    private bodyguardService:BodyguardService
+  ){
+    this.arrBodyguards= [];
+  }
+  ngOnInit(): void {
+    this.bodyguardService.getListObs().subscribe(data=>this.arrBodyguards=data);
+
+    this.bodyguardService.getList().subscribe(data=>this.arrBodyguards=data);
+  }
 
 }
---
+++ Bodyguard/src/app/Services/bodyguard.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../Environments/environment';
+import { Bodyguard } from '../Model/bodyguard';
+import { Observable, Subject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+const base_url = environment.base;
+@Injectable({
+  providedIn: 'root'
+})
+export class BodyguardService {
+  private url = `${base_url}/Bodyguard`;
+  private listBodyguards = new Subject<Bodyguard[]>();
+  constructor(
+    private http:HttpClient
+  ) {
+
+  }
+  getList():Observable<any>{
+    return this.http.get(this.url+"/List")
+  }
+
+  getListObs(){
+      return this.listBodyguards.asObservable();
+  }
+
+
+
+
+
+
+
+}
